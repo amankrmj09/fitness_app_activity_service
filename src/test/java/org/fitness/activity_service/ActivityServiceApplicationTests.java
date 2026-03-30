c89f07088c13e34e0c473915f474c8ef8b8d3c3f
Aman Kumar Maurya <amankrmj@gmail.com>
2026-03-31 00:58:59 +0530

    Update ActivityServiceApplicationTests and add API test cases for activity creation

--- src/test/java/org/fitness/activity_service/ActivityServiceApplicationTests.java
+++ src/test/java/org/fitness/activity_service/ActivityServiceApplicationTests.java
@@ -3,11 +3,10 @@ package org.fitness.activity_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ActivityServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
