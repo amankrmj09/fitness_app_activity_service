3abcee2881a900070ea1860aab12bf70f049f23f
Aman Kumar Maurya <amankrmj@gmail.com>
2026-04-02 09:29:15 +0530

    Add UserClient interface for user validation via Feign

---
+++ src/main/java/org/fitness/activity_service/client/UserClient.java
@@ -0,0 +1,16 @@
+package org.fitness.activity_service.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+    @GetMapping("/user-service/api/v1/users/{uuid}/validate")
+    public Boolean validateUser(@PathVariable String uuid) ;
+}
