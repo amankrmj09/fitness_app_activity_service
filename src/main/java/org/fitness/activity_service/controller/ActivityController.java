18fcf555516fec90e693112c2a85fd536cb6a6c6
Aman Kumar Maurya <amankrmj@gmail.com>
2026-04-02 11:26:27 +0530

    Update ActivityController and application configuration for API path adjustments

--- src/main/java/org/fitness/activity_service/controller/ActivityController.java
+++ src/main/java/org/fitness/activity_service/controller/ActivityController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/activities")
+@RequestMapping("/api/v1/activities")
 @RequiredArgsConstructor
 public class ActivityController {
 
